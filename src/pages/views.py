8b171bdba7041378fed9bd4bdd4b4efe89bbf83d
muktoakash <73410983+muktoakash@users.noreply.github.com>
2023-10-07 22:37:35 -0400

    Explored Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -7,4 +7,13 @@ def home_view(request, *args, **kwargs):
 	return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Contact Page</h1>")
+	return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+	my_context ={
+	"my_text": "This is about Mukto learning Django",
+	"my_number": 123,
+	"my_list": ["item1", "item2", "item3"],
+	"my_html": "<h1>HTML tag</h1>"
+	}
+	return render(request, "about.html", my_context)
