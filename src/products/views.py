eb73c4976213faf3c2f56ced8cb097f416383c6c
muktoakash <73410983+muktoakash@users.noreply.github.com>
2023-10-12 17:19:33 -0400

    Form initialized

--- src/products/views.py
+++ src/products/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
+
+def product_create_view(request):
+	form = ProductForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		form = ProductForm()
+
+	context = {
+	'form': form
+	}
+	return render(request, "products/product_create.html", context)
+
 def product_detail_view(request):
 	obj = Product.objects.get(id=1)
 	# context={
@@ -12,4 +25,4 @@ def product_detail_view(request):
 	context = {
 	'object' : obj
 	}
-	return render(request, "product/detail.html", context)
+	return render(request, "products/detail.html", context)
