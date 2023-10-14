76363d423dcea928cf54aa2c6f3c3bde23d0aefc
muktoakash <73410983+muktoakash@users.noreply.github.com>
2023-10-14 14:58:47 -0400

    Django model forms, From widgets, validation

--- src/products/forms.py
+++ src/products/forms.py
@@ -2,6 +2,18 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+	title = forms.CharField(label='', 
+		widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+	description = forms.CharField(required=False,
+		widget=forms.Textarea(
+			attrs={
+	 			"class": "new-class-name two",
+	 			"id": "my-id-for-textarea",
+	 			"rows": 10,
+	 			"cols": 120,
+	 			"placeholder": "Your description",
+	 	}))
+	price = forms.DecimalField(initial = 199.99)
 	class Meta:
 		model = Product
 		fields = [
@@ -9,3 +21,22 @@ class ProductForm(forms.ModelForm):
 			'description',
 			'price'
 			]
+	def clean_title(self, *args, **kwargs):
+		title = self.cleaned_data.get("title")
+		if not "trydjango" in title:
+			raise forms.ValidationError("This is not a valid title")
+		return title
+
+class RawProductFrom(forms.Form):
+	title = forms.CharField(label='', 
+		widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+	description = forms.CharField(required=False,
+		widget=forms.Textarea(
+			attrs={
+	 			"class": "new-class-name two",
+	 			"id": "my-id-for-textarea",
+	 			"rows": 10,
+	 			"cols": 120,
+	 			"placeholder": "Your description",
+	 	}))
+	price = forms.DecimalField(initial = 199.99)
--- src/products/views.py
+++ src/products/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductFrom
 
 # Create your views here.
 
 def product_create_view(request):
-	if request.method == 'POST':
-		my_new_title = request.POST.get('title')
-		Product.objects.create(title=my_new_title)
-	context={}
-	return render(request, "products/product_create.html", context)
+	# if request.method == 'POST':
+	# 	my_new_title = request.POST.get('title')
+	# 	Product.objects.create(title=my_new_title)
 	
-	# form = ProductForm(request.POST or None)
-	# if form.is_valid():
-	# 	form.save()
-	# 	form = ProductForm()
-
-	# context = {
-	# 'form': form
-	# }
+	# my_form = RawProductFrom()
+	# if request.method == "POST":
+	# 	my_form = RawProductFrom(request.POST)
+	# 	if my_form.is_valid():
+	# 		# now the data is good
+	# 		print(my_form.cleaned_data)
+	# 		Product.objects.create(**my_form.cleaned_data)
+	# 	else:
+	# 		print(my_form.errors)
+	# context={"form": my_form }
 	# return render(request, "products/product_create.html", context)
+	
+	form = ProductForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		form = ProductForm()
+
+	context = {
+	'form': form
+	}
+	return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
 	obj = Product.objects.get(id=1)
